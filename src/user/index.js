2e8914ab8c277d42e731538eb330b50faa04dde9
Tahir Zahid <devtahirzahid@gmail.com>
2023-11-09 23:37:39 +0500

    added comment for model name

--- src/user/index.js
+++ src/user/index.js
@@ -1,4 +1,5 @@
-const User = require("./user.model");
-const UserService = require("./user.service");
+// Gettting Model named User
+const User = require('./user.model');
+const UserService = require('./user.service');
 
 module.exports = UserService(User);
